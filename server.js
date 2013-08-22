68e7b7924912132a0e15d5a7318129316335eba2
MisterStabbeh <cheong.e49@gmail.com>
2013-08-21 22:43:49 -0700

    updated server to look at index.html in views folder

--- server.js
+++ server.js
@@ -1,22 +1,25 @@
 //global parameters
-
 var express = require("express"),
 	app = express(),
 	server = require("http").createServer(app);
 
 //setting up environment for app
 app.set('port', process.env.PORT || 3000);
+app.set('view options', {layout: false});
+app.use(express.logger());
+app.use(express.static(__dirname + '/views'));
 
 
 function start() {
 
 	//app needs to get a route
 	app.get('/', function(req, res) {
-		res.send("hello pandaWeekend!");	
+		res.render('/views/index.html');
 	});
 
 	//starts server
-	server.listen(app.get('port'), function() { console.log("Server is starting at port " + app.get('port')); });
+	server.listen(app.get('port'));
+	console.log("Server is starting at port " + app.get('port'));
 
 }
 
