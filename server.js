3d54b0c78c7d857fc23b0e7d51c45c2fe0f1370f
Steve Cheong <stevenc49@gmail.com>
2013-09-26 23:29:37 -0700

    added players list

--- server.js
+++ server.js
@@ -1,17 +1,16 @@
 //global parameters
 var express = require("express");
 var app = express();
-//server = require("http").createServer(app);
 
 //setting up environment for app
 app.set('port', process.env.PORT || 3000);
 app.set('view options', {layout: false});
-//app.use(express.logger());
 app.use(express.static(__dirname + '/views'));
 
 var socket = require("socket.io");
 var server = app.listen(app.get('port'));
 var io = socket.listen(server);	
+var players = [];
 
 //app needs to get a route
 app.get('/', function(req, res) {
@@ -23,6 +22,30 @@ console.log("Server is starting at port " + app.get('port'));
 
 
 io.sockets.on('connection', function(socket) {
+
+	// when the client tries to connect, this executes
+	socket.on('connect', function() {
+		
+		if(players.length==0) {
+			socket.playerColor = "BLACK";
+			socket.emit("sendColorToClient", socket.playerColor);
+
+			players.push(socket.playerColor);
+		}
+		else if (players.length==1) {
+			socket.playerColor = "WHITE";
+			socket.emit("sendColorToClient", socket.playerColor);
+
+			players.push(socket.playerColor);
+		}
+
+		// when the player disconnects
+		socket.on('disconnect', function() {
+
+			players.length = 0;		//clear player array
+			socket.broadcast.emit('disconnect');
+		});
+	});
 
 	// when the client emits sendMoveToServer, this listens and executes
 	socket.on('sendMoveToServer', function(moveMsg) {
--- views/go.js
+++ views/go.js
@@ -2,6 +2,9 @@
 //firefox mouse event fix from: http://stackoverflow.com/questions/12704686/html5-with-jquery-e-offsetx-is-undefined-in-firefox
 
 function loadGame() {
+
+	var playerColor = null;
+
 	// block size
 	var size = 32;
 
@@ -15,16 +18,6 @@ function loadGame() {
 	var o_gx = 0; 
 	var o_gy = 0;
 
-	// load go board
-	//var imageObj = new Image();
-	
-	//imageObj.onload = function() {
-	//	ctx.drawImage(imageObj, 0, 0, canvas.width, canvas.height);
-	//}
-
-	//imageObj.src = 'http://www.mashbout.com/wp-content/uploads/2013/06/Blank_Go_board.png';
-	//imageObj.src = 'Blank_Go_board.png';
-
 	// State of a grid
 	var gState = {	"EMPTY" : 0,
 					"BLACK" : 1,
@@ -34,18 +27,11 @@ function loadGame() {
 	var w = ~~ (canvas.width / size);
 	var h = ~~ (canvas.height / size);
 
-	//var w = 19;
-	//var h = 19;
-
 	// create empty state array
 	var state = new Array();
-	//var state = [];
-	//state.length = 0;	
 
 	for (var y = 0; y < h; ++y) {
 	    state[y] = new Array(w);
-		//state[y] = [];
-		//state[y].length[w]
 	}
 
 	function Cell(state, x, y) {
@@ -63,14 +49,6 @@ function loadGame() {
 	function clearCell(x, y) {
 		ctx.clearRect((x*size), (y*size), size, size);
 		state[y][x] = gState.EMPTY;
-	}
-
-	function isAdjCell(p, x, y) {
-		// If this cell is in this grid state
-	//	if(state[y][x] == p)
-			return true;
-
-	//	return false;
 	}
 
 	function adjCell(q, x, y) {
@@ -93,9 +71,6 @@ function loadGame() {
 		//mark this state somehow
 		mark[x.toString()+y.toString()] = true;
 
-		//alert(mark[gx.toString()+gy.toString()]);
-		//alert(mark['00']); //if not in hashtable, returns undefined
-
 		//queue.length==0 is similar to queue.empty()
 		while(queue.length!=0) {
 			var t = queue.shift();
@@ -108,8 +83,6 @@ function loadGame() {
 
 			if(t.state == s)
 				adjCell(tmp, t.x, t.y);
-
-			//alert(tmp.length);
 		
 			var u;
 			while (tmp.length > 0) {
@@ -124,7 +97,7 @@ function loadGame() {
 			}
 	
 		}
-		//alert("Captured");
+
 		return true;
 	}
 
@@ -135,9 +108,6 @@ function loadGame() {
 		
 		//mark this state somehow
 		mark[x.toString()+y.toString()] = true;
-
-		//alert(mark[gx.toString()+gy.toString()]);
-		//alert(mark['00']); //if not in hashtable, returns undefined
 
 		//queue.length==0 is similar to queue.empty()
 		while(queue.length!=0) {
@@ -150,8 +120,6 @@ function loadGame() {
 				adjCell(tmp, t.x, t.y);
 				clearCell(t.x, t.y);
 			}
-
-			//alert(tmp.length);
 		
 			var u;
 			while (tmp.length > 0) {
@@ -167,93 +135,6 @@ function loadGame() {
 	
 		}
 	}
-
-	// keyboard event, testing purposes
-	//$(document).keydown(function(e) {
-	//	if(e.which == 76) {
-	//		clearBoard();
-	//});
-
-	//$(canvas).hover(
-		// Create a piece as the mouse goes over an empty cell
-		//function(e) {
-		
-		//var mx, my;
-	
-		//if( (e.offsetX || e.offsetY) == undefined) {
-		//	mx = e.pageX-$('canvas').offset().left;
-		//	my = e.pageY-$('canvas').offset().top;
-		//}
-
-		//alert(mx);
-		//alert(my);
-
-		//var gx = ~~ (mx / size);
-		//var gy = ~~ (my / size);
-
-		//alert(gx);
-		//alert(gy);
-
-		//	ctx.fillRect((gx*size), (gy*size), size, size);
-		//	ctx.fillStyle = 'red';
-		//},
-		// Removes the piece as the mouse goes leaves the empty cell
-		//function(e) {
-		//	clearBoard();
-		//}
-
-	//);
-
-//	$(canvas).mousemove(function(e) {
-//		function fill(oc, ic, gx, gy) {
-//			
-//			// Gradient offset
-//			var grdOffset = (size/4)+1;
-
-//			// Create gradient
-//			var grd = ctx.createRadialGradient((gx * size)+grdOffset, (gy * size)+grdOffset, 1, (gx * size)+grdOffset, (gy * size)+grdOffset, 20)
-//			grd.addColorStop(0, ic);
-//			grd.addColorStop(1, oc);
-
-//			ctx.beginPath();
-//			//ctx.fillRect(gx * size, gy * size, size, size);
-//	   		ctx.arc((gx * size)+16, (gy * size)+16, (size/2)-1, 0, 2 * Math.PI, false);
-//			ctx.fillStyle = grd;
-//			//ctx.fillStyle = s;
-//			ctx.fill();
-//			ctx.closePath(); 
-//		}
-
-//		var mx, my;
-//	
-//		if( (e.offsetX || e.offsetY) == undefined) {
-//			mx = e.pageX-$('canvas').offset().left;
-//			my = e.pageY-$('canvas').offset().top;
-//		}
-
-//		//var o_gx, o_gy;
-
-//		var gx = ~~ (mx / size);
-//		var gy = ~~ (my / size);
-
-//		//alert(o_gx);
-
-//		//alert(gx);
-//		// If the mouse is no longer in the previous cell, clear that cell
-//		if((state[o_gy][o_gx] == gState.EMPTY) || (state[o_gy][o_gx] == undefined)) {
-//			if((gx != o_gx) || (gy != o_gy)) {
-//				clearCell(o_gx, o_gy);
-//				o_gx = gx;
-//				o_gy = gy;
-//			}
-//		
-//		ctx.fillRect((gx*size), (gy*size), size, size);
-//		ctx.fillStyle = 'red';
-//		}		
-//		//fill('black', 'grey', gx, gy);
-
-//		//setTimeout(function(), 1);
-//	});
 
 	// click event, using jQuery for cross-browser convenience
 	$(canvas).click(function(e) {
@@ -278,15 +159,6 @@ function loadGame() {
     	socket.emit('sendMoveToServer', moveMsg);
 
     	drawPiece(gx, gy);
-	});
-
-	// listener, whenever the server emits 'broadcastMove', this executes
-	socket.on('broadcastMove', function(data) {
-		console.log("FROM SERVER: " + data)
-
-		var coordinates = data.split(" ");
-
-		drawPiece(coordinates[0], coordinates[1]);
 	});
 
     // quick fill function to save repeating myself later
@@ -336,14 +208,6 @@ function loadGame() {
 			if(isCaptured(op, gx, gy+1))
 				clearCaptured(op, gx, gy+1);
 
-			//ctx.clearRect((gx * size), (gy * size), size, size);			
-			//clearCell(gx, gy);
-
-			//ctx.clearRect(gx, gy, (gx * size)-1, (gy * size)-1);
-			//setTimeout(function() {
-		    //	fill('black', 'grey', gx, gy)
-				//ctx.clearRect(gx, gy, (gx*size), (gy*size));
-			//}, 1000);
 		} else if(state[gy][gx] == gState.WHITE) {
 			state[gy][gx] = gState.EMPTY;
 			clearCell(gx, gy);
@@ -366,10 +230,29 @@ function loadGame() {
 			
 			if(isCaptured(op, gx, gy+1))
 				clearCaptured(op, gx, gy+1);
-
-	    	//isCaptured(gx, gy);
 		}
 	}
+
+	// on connection to server
+	socket.on('connect', function(){
+		socket.emit('connect');
+	});
+
+	// listener, whenever the server emits 'sendColorToClient', this executes
+	socket.on('sendColorToClient', function(color) {
+		playerColor = color;
+	});
+
+	// listener, whenever the server emits 'broadcastMove', this executes
+	socket.on('broadcastMove', function(data) {
+		var coordinates = data.split(" ");
+		drawPiece(coordinates[0], coordinates[1]);
+	});
+
+	// listener, whenever the server emits 'disconnect', this executes
+	socket.on('disconnect', function(color) {
+		alert("Opponent Disconnected. Game Over.")
+	});
 }
 
 window.addEventListener("load", loadGame, false);
